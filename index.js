df47b7584942928f67c31e14aece55ca931a1e5a
naimulabeer <naimul.h.abeer@gmail.com>
2023-10-19 00:30:43 +0600

    adding brand and brand id get route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,6 +24,28 @@ async function run() {
 
     const productCollection = client.db("productDB").collection("product");
     const categoryCollection = client.db("productDB").collection("category");
+
+    app.get("/product", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/product/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      const query = { brand: brand };
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
+    app.get("/product/:brand/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/product", async (req, res) => {
       const newProduct = req.body;
@@ -75,6 +97,10 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Brand Shop server is running");
+});
 
 app.listen(port, () => {
   console.log(`Brand Shop Server is running on port: ${port}`);
